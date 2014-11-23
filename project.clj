9f641ae9e6026c66d45432d35d16a74a1c833b68
Gonzalo Arreche <gonzaloarreche@gmail.com>
2014-11-23 15:50:50 -0200

    Update clojure dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/grilix/ring-anti-forgery"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
                  [hiccup "1.0.5"]]
